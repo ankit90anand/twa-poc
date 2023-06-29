9faab32a249eca6f004fc31849d214a0c610426d
ankit90anand <121392423+ankit90anand@users.noreply.github.com>
2023-06-29 20:46:07 +0530

    chore: Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -57,4 +57,16 @@ const updateName = async (event) => {
     await self.widgets.updateByInstanceId(event.instanceId, payload);
 }
 
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js'
+);
+
+self.addEventListener('push', () => {
+  event.waitUntil(
+    registration.showNotification("Hello!", {
+      body: "This is a push notification!",
+    })
+  );
+});
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
